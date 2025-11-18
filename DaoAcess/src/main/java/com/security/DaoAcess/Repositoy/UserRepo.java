034bf92a8280dc31971e6ac48399c487b0600ee5
AditiTambat <adititambat2710@gmail.com>
2025-11-18 13:05:22 +0530

    password hashing

---
+++ DaoAcess/src/main/java/com/security/DaoAcess/Repositoy/UserRepo.java
@@ -0,0 +1,16 @@
+package com.security.DaoAcess.Repositoy;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.security.DaoAcess.Entity.UserEntity;
+
+@Repository
+public interface UserRepo extends JpaRepository<UserEntity, Integer> {
+
+	
+	@Query(value="select * from user_entity where user_name= :nm", nativeQuery=true)
+	public UserEntity loadbyname(@Param("nm") String nm);
+}
